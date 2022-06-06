5420864e077fab9a297f84381c0c2bfdb8cb7a4d
Wanleyrooby <alexroob96@gmail.com>
2022-06-06 12:28:31 -0300

    crud finalizado

--- src/main/java/com/generation/zerowaste/controller/TemaController.java
+++ src/main/java/com/generation/zerowaste/controller/TemaController.java
@@ -1,18 +1,25 @@
 package com.generation.zerowaste.controller;
 
+
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.generation.zerowaste.model.Tema;
 import com.generation.zerowaste.repository.TemaRepository;
-
 @RestController
 @RequestMapping("/temas")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
@@ -30,6 +37,30 @@ public class TemaController {
     			.map(resposta -> ResponseEntity.ok(resposta))
     			.orElse(ResponseEntity.notFound().build());
     }
+    @GetMapping("/titulo/{titulo_tema}")
+    public ResponseEntity<List<Tema>> getByTitulo(@PathVariable String titulo_tema) {
+    	return ResponseEntity.ok(temaRepository.findAllByTituloContainingIgnoreCase(titulo_tema));
+    }
+    @PostMapping
+    public ResponseEntity<Tema> postTema(@Valid @RequestBody Tema tema) {
+    	return ResponseEntity.status(HttpStatus.CREATED).body(temaRepository.save(tema));
+    }
+    @PutMapping
+    public ResponseEntity<Tema> putTema(@Valid @RequestBody Tema tema) {
+    	return temaRepository.findById(tema.getId()) 
+    			.map(resposta -> ResponseEntity.ok(resposta))
+    			.orElse(ResponseEntity.notFound().build());
+    			
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Tema> deleteTema(@PathVariable Long id) {
+    	if(temaRepository.existsById(id)) {
+    		temaRepository.deleteById(id);
+    		return ResponseEntity.noContent().build(); 
+    	}
+    	return ResponseEntity.notFound().build();
+    }
+    
     
     
     
--- src/main/java/com/generation/zerowaste/repository/TemaRepository.java
+++ src/main/java/com/generation/zerowaste/repository/TemaRepository.java
@@ -1,10 +1,14 @@
 package com.generation.zerowaste.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.generation.zerowaste.model.Tema;
 @Repository
 public interface TemaRepository   extends JpaRepository<Tema,Long>{
+	public List <Tema> findAllByTituloContainingIgnoreCase(@Param("titulo") String titulo_tema);  
 
 }
